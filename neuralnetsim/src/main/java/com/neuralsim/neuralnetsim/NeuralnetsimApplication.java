b32ea5fd295a853a9c1cb2d26dcf5cc9997da3af
Xiaolequan <stevenli2088@hotmail.com>
2023-07-22 21:01:11 -0700

    Initialization
    Spring webservices

---
+++ neuralnetsim/src/main/java/com/neuralsim/neuralnetsim/NeuralnetsimApplication.java
@@ -0,0 +1,13 @@
+package com.neuralsim.neuralnetsim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuralnetsimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeuralnetsimApplication.class, args);
+	}
+
+}
---
+++ neuralnetsim/src/test/java/com/neuralsim/neuralnetsim/NeuralnetsimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neuralsim.neuralnetsim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuralnetsimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
